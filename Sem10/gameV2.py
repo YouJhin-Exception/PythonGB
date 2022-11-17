93695713e5f9e4456eaecf93b39d6f9fad36dae8
«YouJhin» <palevich86@mail.ru>
2022-11-17 12:44:56 +0300

    add Sem_10 :calling:

--- Sem10/gameV2.py
+++ Sem10/gameV2.py
@@ -4,15 +4,11 @@ from tok import tok
 import emoji
 from loging import *
 import random
-import newOut as no
-import newOutCross as noc
-
 
 bot = Bot(tok)
 dp = Dispatcher(bot)
 print('Server startup')
 
-#--------------------------------------------------------------
 item = {}
 
 gameIsStart = False
@@ -21,31 +17,6 @@ gameGround = [" ", " ", " ",
               " ", " ", " ", ]
 botSymbol = emoji.emojize(':cross_mark:')
 playerSymbol = emoji.emojize(':hollow_red_circle:')
-
-winbool = False
-losebool = False
-
-def clear():
-    global gameGround
-    gameGround = [" ", " ", " ",
-                  " ", " ", " ",
-                  " ", " ", " ", ]
-def win(cell_1, cell_2, cell_3):
-    if cell_1 == playerSymbol and cell_2 == playerSymbol and cell_3 == playerSymbol:
-        global winbool
-        winbool = True
-
-def lose(cell_1, cell_2, cell_3):
-    if cell_1 == botSymbol and cell_2 == botSymbol and cell_3 == botSymbol:
-        global losebool
-        losebool = True
-#______________________________________________________________
-
-
-
-#CrossesOrToe = [emoji.emojize(':hollow_red_circle:'), emoji.emojize(':cross_mark:')]
-
-
 #playerSymbol = CrossesOrToe[random.randint(0, 1)]
 
 # botSymbol = ""
@@ -58,7 +29,10 @@ def lose(cell_1, cell_2, cell_3):
 #     if cell_1 == playerSymbol and cell_2 == playerSymbol:
 #         posDef = botSymbol
 
-calc_mark = types.InlineKeyboardMarkup()
+winbool = False
+losebool = False
+
+calc_mark = types.InlineKeyboardMarkup(resize_keyboard=True)
 calc_mark.row(types.InlineKeyboardButton("CE", callback_data="CE"),
               types.InlineKeyboardButton("C", callback_data="C"),
               types.InlineKeyboardButton("<=", callback_data="<="),
@@ -79,15 +53,34 @@ calc_mark.row(types.InlineKeyboardButton("1", callback_data="1"),
               types.InlineKeyboardButton("3", callback_data="3"),
               types.InlineKeyboardButton("+", callback_data="+"))
 
-calc_mark.row(types.InlineKeyboardButton(" ", callback_data="no"),
+calc_mark.row(types.InlineKeyboardButton("j", callback_data="j"),
               types.InlineKeyboardButton("0", callback_data="0"),
               types.InlineKeyboardButton(",", callback_data="."),
               types.InlineKeyboardButton("=", callback_data="="))
 
+calc_mark.row(types.InlineKeyboardButton("(", callback_data="("),
+              types.InlineKeyboardButton(")", callback_data=")"))
+
+
 value = ''
 oldvalue = ''
 
+typeS = None
 
+def clear():
+    global gameGround
+    gameGround = [" ", " ", " ",
+                  " ", " ", " ",
+                  " ", " ", " ", ]
+def win(cell_1, cell_2, cell_3):
+    if cell_1 == playerSymbol and cell_2 == playerSymbol and cell_3 == playerSymbol:
+        global winbool
+        winbool = True
+
+def lose(cell_1, cell_2, cell_3):
+    if cell_1 == botSymbol and cell_2 == botSymbol and cell_3 == botSymbol:
+        global losebool
+        losebool = True
 
 @dp.message_handler(commands=['go'])
 async def send_welcome(message: types.Message):
@@ -111,19 +104,43 @@ async def c_time(message: types.Message):
     await log_g2(message)
 
 
+@dp.message_handler(Text(equals=[emoji.emojize(':video_game:play')]))
+async def c_play(message: types.Message):
+    global gameIsStart
+    clear()
+    gameIsStart = True
 
-#@dp.message_handler(Text(equals = [emoji.emojize(':mobile_phone:calc')]), no.new_calc)
+    if gameIsStart == True:
+        i = 0
+        item = {}
+        global game_mark
+        game_mark = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=3)
+        # item[0] = types.InlineKeyboardButton(gameGround[0], callback_data="cell0")
+        # item[1] = types.InlineKeyboardButton(gameGround[0], callback_data="cell1")
+        # item[2] = types.InlineKeyboardButton(gameGround[0], callback_data="cell2")
+        # item[3] = types.InlineKeyboardButton(gameGround[0], callback_data="cell3")
+        # item[4] = types.InlineKeyboardButton(gameGround[0], callback_data="cell4")
+        # item[5] = types.InlineKeyboardButton(gameGround[0], callback_data="cell5")
+        # item[6] = types.InlineKeyboardButton(gameGround[0], callback_data="cell6")
+        # item[7] = types.InlineKeyboardButton(gameGround[0], callback_data="cell7" + str(i))
 
-global cho
-cho = "game"
+        i = 0
+
+        for i in range(9):
+            item[i] = types.InlineKeyboardButton(gameGround[0], callback_data="cell" + str(i))
+
+        game_mark.row(item[0], item[1], item[2])
+        game_mark.row(item[3], item[4], item[5])
+        game_mark.row(item[6], item[7], item[8])
+        await bot.send_message(message.chat.id, emoji.emojize(':cross_mark:   Выбери клетку   :hollow_red_circle:'),
+                               reply_markup=game_mark)
+        global typeS
+        typeS = "game"
+        await log_g2(message)
 
 
 @dp.message_handler(Text(equals = [emoji.emojize(':mobile_phone:calc')]))
 async def new_calc(message: types.Message):
-    global cho
-    print(cho)
-    cho = 'calc'
-    print(cho)
     # await message.reply(emoji.emojize('ааа, мы хотим посчитать :man_technologist:'))
     # calc_chose_mark = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=2)
     # calc_chose_mark.row(types.InlineKeyboardButton('рациональные', callback_data="ratio"),
@@ -136,41 +153,73 @@ async def new_calc(message: types.Message):
         await bot.send_message(message.chat.id, '0', reply_markup=calc_mark)
     else:
         await bot.send_message(message.chat.id, value, reply_markup=calc_mark)
+    global typeS
+    typeS = "calc"
+    await log_g2(message)
 
 
-
-#@dp.message_handler(Text(equals = [emoji.emojize(':video_game:play')]), noc.c_play)
-#@dp.callback_query_handler(Text(equals="yes"), noc.callbackInline)
-
-
-
-
-@dp.message_handler(Text(equals = [emoji.emojize(':video_game:play')]))
-async def c_play(message: types.Message):
-
-    global gameIsStart
-    clear()
-    gameIsStart = True
-
-    if gameIsStart == True:
-        item = {}
-        global markup
-        markup = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=3)
-
-        i = 0
-
+@dp.callback_query_handler()
+async def callbackInline(call):
+    print(typeS)
+    if typeS == "game":
         for i in range(9):
-            item[i] = types.InlineKeyboardButton(gameGround[i], callback_data=str(i))
+            if call.data == "cell" + str(i):
+                print(i)
+                if (gameGround[i] == " "):
+                    gameGround[i] = playerSymbol
+                randomCell = random.randint(0, 8)
+                if gameGround[randomCell] == playerSymbol:
+                    randomCell = random.randint(0, 8)
+                if gameGround[randomCell] == botSymbol:
+                    randomCell = random.randint(0, 8)
+                if gameGround[randomCell] == " ":
+                    gameGround[randomCell] = botSymbol
 
-        markup.row(item[0], item[1], item[2])
-        markup.row(item[3], item[4], item[5])
-        markup.row(item[6], item[7], item[8])
-        await bot.send_message(message.chat.id, emoji.emojize(':cross_mark:   Выбери клетку   :hollow_red_circle:'),
-                               reply_markup=markup)
+                for i in range(9):
+                    item[i] = types.InlineKeyboardButton(gameGround[i], callback_data="cell" + str(i))
+                global markup
+                markup = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=3)
+                markup.row(item[0], item[1], item[2])
+                markup.row(item[3], item[4], item[5])
+                markup.row(item[6], item[7], item[8])
+                await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id,
+                                                    reply_markup=markup)
 
-if cho == "calc":
-    @dp.callback_query_handler(lambda call: call)
-    async def callbackCalc(call):
+                win(gameGround[0], gameGround[1], gameGround[2])
+                win(gameGround[3], gameGround[4], gameGround[5])
+                win(gameGround[6], gameGround[7], gameGround[8])
+                win(gameGround[0], gameGround[3], gameGround[6])
+                win(gameGround[1], gameGround[4], gameGround[7])
+                win(gameGround[2], gameGround[5], gameGround[8])
+                win(gameGround[0], gameGround[4], gameGround[8])
+                win(gameGround[2], gameGround[4], gameGround[6])
+
+                lose(gameGround[0], gameGround[1], gameGround[2])
+                lose(gameGround[3], gameGround[4], gameGround[5])
+                lose(gameGround[6], gameGround[7], gameGround[8])
+                lose(gameGround[0], gameGround[3], gameGround[6])
+                lose(gameGround[1], gameGround[4], gameGround[7])
+                lose(gameGround[2], gameGround[5], gameGround[8])
+                lose(gameGround[0], gameGround[4], gameGround[8])
+                lose(gameGround[2], gameGround[4], gameGround[6])
+
+                global winbool
+                global gameIsStart
+                if winbool:
+                    clear()
+                    await bot.send_message(call.message.chat.id, emoji.emojize(' Я проиграл :loudly_crying_face:'))
+                    winbool = False
+                    gameIsStart = False
+                global losebool
+                if losebool:
+                    clear()
+                    await bot.send_message(call.message.chat.id,
+                                           emoji.emojize(' Я победил!!! :winking_face_with_tongue::victory_hand:'))
+                    losebool = False
+                    gameIsStart = False
+
+
+    elif typeS == "calc":
         global value, oldvalue
         data = call.data
 
@@ -205,66 +254,6 @@ if cho == "calc":
             value = ''
 
 
-elif cho == "game":
-    @dp.callback_query_handler()
-    async def callbackInline(call):
-        if (call.message):
-
-            for i in range(9):
-                if call.data == str(i):
-                    print(i)
-                    if (gameGround[i] == " "):
-                        gameGround[i] = playerSymbol
-            randomCell = random.randint(0, 8)
-            if gameGround[randomCell] == playerSymbol:
-                randomCell = random.randint(0, 8)
-            if gameGround[randomCell] == botSymbol:
-                randomCell = random.randint(0, 8)
-            if gameGround[randomCell] == " ":
-                gameGround[randomCell] = botSymbol
-
-            for i in range(9):
-                item[i] = types.InlineKeyboardButton(gameGround[i], callback_data=str(i))
-            global markup
-            markup = types.InlineKeyboardMarkup(resize_keyboard=True, row_width=3)
-            markup.row(item[0], item[1], item[2])
-            markup.row(item[3], item[4], item[5])
-            markup.row(item[6], item[7], item[8])
-            await bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id,
-                                                reply_markup=markup)
-
-            win(gameGround[0], gameGround[1], gameGround[2])
-            win(gameGround[3], gameGround[4], gameGround[5])
-            win(gameGround[6], gameGround[7], gameGround[8])
-            win(gameGround[0], gameGround[3], gameGround[6])
-            win(gameGround[1], gameGround[4], gameGround[7])
-            win(gameGround[2], gameGround[5], gameGround[8])
-            win(gameGround[0], gameGround[4], gameGround[8])
-            win(gameGround[2], gameGround[4], gameGround[6])
-
-            lose(gameGround[0], gameGround[1], gameGround[2])
-            lose(gameGround[3], gameGround[4], gameGround[5])
-            lose(gameGround[6], gameGround[7], gameGround[8])
-            lose(gameGround[0], gameGround[3], gameGround[6])
-            lose(gameGround[1], gameGround[4], gameGround[7])
-            lose(gameGround[2], gameGround[5], gameGround[8])
-            lose(gameGround[0], gameGround[4], gameGround[8])
-            lose(gameGround[2], gameGround[4], gameGround[6])
-
-            global winbool
-            global gameIsStart
-            if winbool:
-                clear()
-                await bot.send_message(call.message.chat.id, emoji.emojize(' Я проиграл :loudly_crying_face:'))
-                winbool = False
-                gameIsStart = False
-            global losebool
-            if losebool:
-                clear()
-                await bot.send_message(call.message.chat.id,
-                                       emoji.emojize(' Я победил!!! :winking_face_with_tongue::victory_hand:'))
-                losebool = False
-                gameIsStart = False
 
 
 # @dp.message_handler()                             - эхо
